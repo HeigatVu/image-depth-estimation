ee6c0c7b7157d3be377cfef3b1f72604de5186c9
HeigatVu <vnmduc6420@gmail.com>
2024-08-21 00:05:19 +0700

    finish test

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ import pixel_wise_matching_l1
 import pixel_wise_matching_l2
 import window_based_matching_l1
 import window_based_matching_l2
+import window_based_matching_cosine
 
 left_img_path = "/home/heigatvu/my-project/AIO2024/strereo-matching/data/tsukuba/left.png"
 right_img_path = "/home/heigatvu/my-project/AIO2024/strereo-matching/data/tsukuba/right.png"
 disparity_range = 16
 
 left_img_path_2 = "/home/heigatvu/my-project/AIO2024/strereo-matching/data/Aloe/Aloe_left_1.png"
-right_img_path_2 = "/home/heigatvu/my-project/AIO2024/strereo-matching/data/Aloe/Aloe_right_2.png"
+right_img_path_2 = "/home/heigatvu/my-project/AIO2024/strereo-matching/data/Aloe/Aloe_right_3.png"
 disparity_range_2 = 64
 kernel_size = 5
 
@@ -25,8 +26,11 @@ kernel_size = 5
 #     left_img_path_2, right_img_path_2, disparity_range_2, kernel_size, save_result=True)
 
 
-window_based_matching_l2.window_based_matching_l2(
-    left_img_path_2, right_img_path_2, disparity_range_2, kernel_size, save_result=True)
+# window_based_matching_l2.window_based_matching_l2(
+#     left_img_path_2, right_img_path_2, disparity_range_2, kernel_size, save_result=True)
 
-window_based_matching_l1.window_based_matching_l1(
+# window_based_matching_l1.window_based_matching_l1(
+#     left_img_path_2, right_img_path_2, disparity_range_2, kernel_size, save_result=True)
+
+window_based_matching_cosine.window_based_matching_cosine_similarity(
     left_img_path_2, right_img_path_2, disparity_range_2, kernel_size, save_result=True)
